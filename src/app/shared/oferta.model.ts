03d6350b1cedfed73e0f73ede9cae9610f1f7809
Bianca Franco <95880578+biancafranco1@users.noreply.github.com>
2023-05-15 19:50:05 -0300

    Add files via upload

---
+++ src/app/shared/oferta.model.ts
@@ -0,0 +1,11 @@
+export class Oferta {
+
+  public id: any
+  public categoria: any
+  public titulo: any
+  public descricao_oferta: any
+  public anunciante: any
+  public valor: any
+  public destaque: any
+  public imagens: any
+}
